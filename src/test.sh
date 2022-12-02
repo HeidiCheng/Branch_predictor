4d16be3ff9f1ab94171eef971e737d6138349b95
HeidiCheng <heidi8844@gmail.com>
2022-12-01 20:54:41 -0800

    add script for testing

---
+++ src/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+make
+
+tracesFolder="../traces/"
+traces=("fp_1.bz2" "fp_2.bz2" "int_1.bz2" "int_2.bz2" "mm_1.bz2" "mm_2.bz2")
+
+for file in ${traces[@]}; do
+    tracePath="$tracesFolder$file"
+    echo "Test on $file"
+    echo -e "\nStatic"
+    bunzip2 -kc $tracePath | ./predictor --static
+    echo -e "\nGshare"
+    bunzip2 -kc $tracePath | ./predictor --gshare:13
+    echo -e "\nTournament"
+    bunzip2 -kc $tracePath | ./predictor --tournament:9:10:10
+    # echo "Custom"
+    # ./predictor --custom $tracePath
+    # bunzip2 -kc $tracePath | ./predictor --custom
+done
